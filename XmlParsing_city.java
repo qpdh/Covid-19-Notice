0bf99cb8b26549e5ea850b3d57cb992aae76042a
tkdans7589 <32192086+tkdans7589@users.noreply.github.com>
2021-05-04 17:04:44 +0900

    Create XmlParsing_city.java

---
+++ XmlParsing_city.java
@@ -0,0 +1,68 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+public class XmlParsing_city {
+
+	private static String getTagValue(String tag, Element eElement) {
+		try{
+	        String result = eElement.getElementsByTagName(tag).item(0).getTextContent();
+	        return result;
+	    } catch(NullPointerException e){
+	        return "";
+	    } catch(Exception e){
+	        return "";
+	    }
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+		String today = sdf.format(date);
+		String startCreateDt = today;
+		String endCreateDt = today;
+		
+		try {
+			
+			String url = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=rl%2B8bqQgAXlgml1MRoJIqGc1YcMKT31NQdmV2graSOPOnxBBdSAAtnKp%2F7XR54yLXVpvKhTnv7UhUw%2FTBjqw9Q%3D%3D&pageNo=1&numOfRows=40&startCreateDt=" + startCreateDt + "&endCreateDt="+ endCreateDt;
+
+			DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
+			Document doc = dBuilder.parse(url);
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("item");
+			Node nNode = nList.item(0);
+			Element eElement_date = (Element) nNode;
+
+			System.out.println("기준일 : " + getTagValue("stdDay", eElement_date));
+			
+			for(int temp = 1; temp < nList.getLength() - 1; temp++){		
+				nNode = nList.item(temp);
+				if(nNode.getNodeType() == Node.ELEMENT_NODE){
+									
+					Element eElement = (Element) nNode;
+					System.out.println("######################");
+					//System.out.println("기준일시 : " + getTagValue("stdDay", eElement));
+					System.out.println("시도명 : " + getTagValue("gubun", eElement));
+					System.out.println("누적 확진자 수  : " + getTagValue("defCnt", eElement));
+					System.out.println("사망자 수 : " + getTagValue("deathCnt", eElement));
+					System.out.println("전일대비 증감 수 : " + getTagValue("incDec", eElement));
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			}	
+	}
+
+}
