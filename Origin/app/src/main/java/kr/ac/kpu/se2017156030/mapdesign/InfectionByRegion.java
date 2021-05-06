ef1d6b52197f66290e43375a9755027029c9e0a6
Nerbe72 <81239318+Nerbe72@users.noreply.github.com>
2021-05-06 22:56:31 +0900

    그래프 추가

---
+++ Origin/app/src/main/java/kr/ac/kpu/se2017156030/mapdesign/InfectionByRegion.java
@@ -0,0 +1,15 @@
+package kr.ac.kpu.se2017156030.mapdesign;
+
+public class InfectionByRegion {
+    String gubun;
+    String def_cnt;
+    String inc_dec;
+    String stdDay;
+
+    InfectionByRegion(String gubun, String def_cnt, String inc_dec, String std_day) {
+        this.gubun = gubun;
+        this.def_cnt = def_cnt;
+        this.inc_dec = inc_dec;
+        this.stdDay = std_day;
+    }
+}
